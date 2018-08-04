fbd8946cb304b4185256dfaa8e16865b191f8f8d
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-08-04 13:55:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file
+function findMatching (arr, wanted)
+{
+  return arr.filter(function(driverName)
+  { return driverName.toLowerCase() === wanted.toLowerCase(); }) ;
+}
+
+function fuzzyMatch (arr, letters)
+{
+  let letterLength = letters.length;
+  return arr.filter(function(drivers){
+    return drivers.slice(0, letterLength) === letters
+  })
+}
+
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+
+
+function matchName(arr, name)
+{
+ return arr.filter(function(items){
+    return items.name.toLowerCase() === name.toLowerCase();
+  });
+}
+
+
+
